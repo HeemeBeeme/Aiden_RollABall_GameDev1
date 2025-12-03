e495d58b1c6b99e0c55d3722e5dec47893bd323d
Heeme Beeme <heemebeeme@gmail.com>
2025-12-03 17:47:08 -0400

    slightly updated tutorial

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -73,48 +73,54 @@ public class TutorialManager : MonoBehaviour
 
             case 4:
 
-                TutorialText.text = "Wow, So Cool! Now, if you look In The Bottom Left Corner You'll Notice A Little Button, Go Ahead Click On It...";
+                TutorialText.text = "If You Broke A Golden Rock, Congratulations! These Rocks Are Far More Rare Than Regular Rocks... They Tend To Give More Gems, But They Aren't Always So Generous!";
 
                 break;
 
             case 5:
 
-                TutorialText.text = "This Is The Sidebar, And Where You Will Find How Much Money And How Many Gems You Have!";
+                TutorialText.text = "Now, if you look In The Bottom Left Corner You'll Notice A Little Button, Go Ahead Click On It...";
 
                 break;
 
             case 6:
 
-                TutorialText.text = "You Will Also Find Upgrades, And A Way To Level Up!";
+                TutorialText.text = "This Is The Sidebar, And Where You Will Find How Much Money And How Many Gems You Have!";
 
                 break;
 
             case 7:
 
-                TutorialText.text = "You Can Also Close The Sidebar With the Button On The Top Right Of The Sidebar";
+                TutorialText.text = "You Will Also Find Upgrades, And A Way To Level Up!";
 
                 break;
 
             case 8:
 
-                TutorialText.text = "And I Must Warn You... There Is A Dangerous Creature Around Here Named... HIM! Why? Yeah I Don't Know And Don't Care.";
+                TutorialText.text = "You Can Also Close The Sidebar With the Button On The Top Right Of The Sidebar";
 
                 break;
 
             case 9:
+
+                TutorialText.text = "And I Must Warn You... There Is A Dangerous Creature Around Here Named... HIM! Why? Yeah I Don't Know And Don't Care.";
+
+                break;
+
+            case 10:
 
                 TutorialText.text = "Alright Well He's Out To Get You... Better Run";
                 Enemy.SetActive(true);
 
                 break;
 
-            case 10:
+            case 11:
 
                 TutorialText.text = "Now Go Out And Break Some Rocks, Collect Some Gems, And Make Some MONEY!";
 
                 break;
 
-            case 11:
+            case 12:
 
                 TutorialText.text = "Oh And You Can Pause Using The Esc Button!";
                 playerController.CanPause = true;
